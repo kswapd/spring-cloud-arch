c4a06e270cb422e026b812cc858af82148132dd2
kongxiangwen <xw.kong@hnair.com>
2018-05-07 11:03:15 +0800

    add eureka module

---
+++ src/main/java/main/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by kongxiangwen on 5/7/18 w:19.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
+
