19bd5e573d13aa5ba07f020d065b620a02dffc19
kongxiangwen <xw.kong@hnair.com>
2018-05-07 14:59:15 +0800

    add service provider

---
+++ hello-service/src/main/java/controllers/HelloController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+
+
+import java.util.List;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by kongxiangwen on 5/7/18 w:19.
+ */
+
+@RestController
+public class HelloController {
+	//private final Logger logger = Logger.getLogger(HelloController.class);
+	@Autowired
+	private DiscoveryClient client;
+
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String index() {
+
+		List<ServiceInstance> instances = client.getInstances("hello-service");
+		for (int i = 0; i < instances.size(); i++) {
+			//logger.info("/hello,host:" + instances.get(i).getHost() + ",service_id:" + instances.get(i).getServiceId());
+		}
+		return "Hello World";
+	}
+}
---
+++ hello-service/src/main/java/main/ProviderApplication.java
@@ -0,0 +1,18 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by kongxiangwen on 5/7/18 w:19.
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderApplication.class, args);
+	}
+}
