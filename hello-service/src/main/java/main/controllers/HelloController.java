700d49fb66ed8e3c4032fc8a38119d108b41e59a
kongxiangwen <xw.kong@hnair.com>
2018-05-07 15:23:23 +0800

    move hello-world package

--- hello-service/src/main/java/main/controllers/HelloController.java
+++ hello-service/src/main/java/main/controllers/HelloController.java
@@ -1,11 +1,8 @@
-package controllers;
+package main.controllers;
 
 
 
-import java.util.List;
-import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,10 +22,10 @@ public class HelloController {
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String index() {
 
-		List<ServiceInstance> instances = client.getInstances("hello-service");
-		for (int i = 0; i < instances.size(); i++) {
+		//List<ServiceInstance> instances = client.getInstances("hello-service");
+		//for (int i = 0; i < instances.size(); i++) {
 			//logger.info("/hello,host:" + instances.get(i).getHost() + ",service_id:" + instances.get(i).getServiceId());
-		}
+		//}
 		return "Hello World";
 	}
 }
